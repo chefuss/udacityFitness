37f252e9f2f9289c41d0a11ee32f1dcd6b141a7b
chefuss <mfernandaroque@gmail.com>
2020-01-16 23:44:05 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import History from './components/History'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{flex:1}}>
+          <View style={{height: 20}}/>
+          <History />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App
